562132f56cdcec1a3a25decca23263b55923223c
wss-git <wssgryx@163.com>
2020-11-03 16:00:30 +0800

    sync help

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ class SyncComponent extends Component {
   constructor() {
     super();
   }
+
+  async sync (inputs) {
+    this.help(inputs, getHelp(inputs));
+
+    const {
+      Properties: properties = {},
+      Credentials: credentials = {}
+    } = inputs;
+
+    const {
+      Region: region,
+      Service: serviceProp = {},
+      Function: functionProp = {}
+    } = properties;
+    const serviceName = serviceProp.Name;
+    const functionName = functionProp.Name;
+
+    const { Commands: commands, Parameters: parameters } = this.args(inputs.Args);
+
+  }
 }
 
 module.exports = SyncComponent;
--- utils/help.js
+++ utils/help.js
@@ -1,6 +1,26 @@
 
 module.exports = (inputs) => ({
-  description: `Usage: s ${inputs.Project.ProjectName} metrics
+  description: `Usage: s ${inputs.Project.ProjectName} sync
 
-  Monitoring function indicators.`
+    Synchronize remote configuration.`,
+  commands: [{
+    name: 'service',
+    desc: 'only sync service.'
+  }, {
+    name: 'tags',
+    desc: 'only sync service tags.'
+  }, {
+    name: 'function',
+    desc: 'only sync function config.'
+  }, {
+    name: 'code',
+    desc: 'only sync function code.'
+  }, {
+    name: 'trigger',
+    desc: 'only sync trigger.'
+  }],
+  args: [{
+    name: '--save <filePath>',
+    desc: 'Sync the configuration file save path.'
+  }]
 })
